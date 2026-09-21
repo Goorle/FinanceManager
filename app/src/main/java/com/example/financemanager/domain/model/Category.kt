0c5f83875ae5b3b31864e8a71f54bdc28375d888
Goorle <ank.daniil@yandex.ru>
2026-09-21 21:58:44 +0300

    Model category

---
+++ app/src/main/java/com/example/financemanager/domain/model/Category.kt
@@ -0,0 +1,18 @@
+package com.example.financemanager.domain.model
+
+import com.example.financemanager.R
+
+enum class Category(
+    val displayName: String,
+    val icon: Int
+) {
+    FOOD("Food", R.drawable.food_vector),
+    TRANSPORT("Transport", R.drawable.transport_vector),
+    MEDICINE("Medicine", R.drawable.medicine_vector),
+    GROCERIES("Groceries", R.drawable.products_vector),
+    RENT("Rent", R.drawable.rent_vector),
+    GIFTS("Gifts", R.drawable.presents_vector),
+    SAVINGS("Savings", R.drawable.savings_vector),
+    ENTERTAINMENT("Entertainment", R.drawable.entertainment_vector),
+    MORE("More", R.drawable.plus_vector)
+}
