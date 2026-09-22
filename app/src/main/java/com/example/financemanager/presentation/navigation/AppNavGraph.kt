df34558aed7904b190db6f0757fb77ebb5c6f1e1
Goorle <ank.daniil@yandex.ru>
2026-09-22 22:04:02 +0300

    new screen Category Details

--- app/src/main/java/com/example/financemanager/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/financemanager/presentation/navigation/AppNavGraph.kt
@@ -4,7 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import com.example.financemanager.domain.model.Category
 import com.example.financemanager.presentation.categories.CategoriesScreen
+import com.example.financemanager.presentation.categories.CategoryDetails
 import com.example.financemanager.presentation.home.HomeScreen
 
 @Composable
@@ -21,12 +24,16 @@ fun AppNavGraph(
         composable<RoutesScreen.Categories> {
             CategoriesScreen(
                 onClickCategory = { category ->
-                    appNavController.navigate(RoutesScreen.CategoryDetails(category.name))
+                    appNavController.navigate(RoutesScreen.CategoryDetails(category))
                 }
             )
         }
+        composable<RoutesScreen.CategoryDetails> { backStackEntry ->
+            val args = backStackEntry.toRoute<RoutesScreen.CategoryDetails>()
+            CategoryDetails(args.category)
+        }
         composable<RoutesScreen.Profile> {  }
         composable<RoutesScreen.Analysis>{}
-        composable<RoutesScreen.Transaction>{;}
+        composable<RoutesScreen.Transaction>{}
     }
 }
